4d01eacfa096be6e67a7b10c9861e2433bc30ac6
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-08 14:04:05 -0500

    Testing MVP complete

--- advanced-forms/cypress/integration/form_test.js
+++ advanced-forms/cypress/integration/form_test.js
@@ -6,5 +6,22 @@ describe("Testing my form", function() {
         cy.get("#name")
             .type("Tristan")
             .should("have.value", "Tristan");
+        cy.get("#email")
+            .type("tristangrovender@yahoo.com")
+            .should("have.value", "tristangrovender@yahoo.com");
+        cy.get("#password")
+            .type("123")
+            .should("have.value", "123");
+        cy.get(".terms > input")
+            .check()
+            .should("be.checked");
+        cy.get("button").click();
+    });
+    it("Test for invalid fields", function() {
+        // cy.get("input:invalid").should("have.length", 0);
+        cy.get("#email").type("not_an_email");
+        cy.get("button")
+            // .click()
+            .should("be.disabled");
     });
 });
